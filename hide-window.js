0dfa0a2d51a7adf97a53acddf74c0dbbc7df380d
Ron Masas <ronmasas@gmail.com>
2024-02-14 17:22:11 +0200

    Update hide-window.js

--- hide-window.js
+++ hide-window.js
@@ -1,6 +1,5 @@
 chrome.windows.getAll({}, async (windows) => {
-    for (let w of windows){
-      chrome.windows.update(w.id, {width: 200, height: 100});
-      chrome.windows.update(w.id, {left: -550, top: 1000});
-    }
+  for (let w of windows) {
+    chrome.windows.update(w.id, { state: "minimized" });
+  }
 });
